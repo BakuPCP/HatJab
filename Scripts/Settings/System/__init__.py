9b86927bf5c28afa14bc314ebab0bd1561fa3388
Pycharm 4pedix <yundermbich@gmail.com>
2025-04-12 22:42:28 +0300

    remake nodes

--- Scripts/Settings/System/__init__.py
+++
@@ -1,7 +0,0 @@
-from .graph_manager import GraphManager
-from .node_editor import NodeEditor
-from .utils import save_positions, load_positions
-from .sisettings import SISettingsEditor
-from .algoritm import AlgorithmVisualizer
-
-__all__ = ['GraphManager', 'NodeEditor', 'save_positions', 'load_positions', 'SISettingsEditor', 'AlgorithmVisualizer']
