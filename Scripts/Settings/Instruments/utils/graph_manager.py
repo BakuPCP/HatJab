e5ebdebf0c790e66b9334d78b98f0f74fe11aee5
Pycharm 4pedix <yundermbich@gmail.com>
2025-04-17 18:50:58 +0300

    123

--- Scripts/Settings/Instruments/utils/graph_manager.py
+++ Scripts/Settings/Instruments/utils/graph_manager.py
@@ -10,17 +10,21 @@ class GraphManager:
     def init_default_graph(self):
         """Инициализация графа по умолчанию"""
         # Основные узлы
-        main_nodes = ["System", "mods", "Scripts", "Data", "main"]
+        main_nodes = ["System", "mods", "Scripts", "Data", "main", "Settings", "Buttons", "Instriments", "utils"]
         for node in main_nodes:
             self.graph.add_node(node, type="folder")
 
         # Файлы как узлы
         file_nodes = {
-            "System": ["crypto.py", "hjcheck.py"],
+           "System": ["crypto.py", "hjcheck.py"],
             "mods": ["example.mod", "weather.mod"],
-            "Scripts": ["commands.txt", "settings.cfg"],
-            "Data": ["keys.dat", "session.ses"],
-            "main": ["main.py"]
+            "Scripts": ["commands.txt", "settings.cfg", "__init__.py", "launch.py", "graph.settings.json"],
+            "Data": ["keys.dhj", "session.seshj", "ps.dhj"],
+            "main": ["main.py"],
+            "Settings": ["__init__.py", "settings.py", "Buttons", "Instruments"],
+            "Buttons" : ["__init.py", "save_settings.py"],
+            "Instruments": ["utils", "__init.py", "click_s.py", "highlight.py", "move.py"],
+            "utils": ["__init__.py", "algo_s.py", "debug_m.py", "graph_manager.py", "help_move.py", "nt_help.py"],
         }
 
         # Добавляем файлы и связи
@@ -32,7 +36,9 @@ class GraphManager:
         # Основные связи между папками
         self.graph.add_edges_from([
             ("main", "System"), ("main", "mods"),
-            ("main", "Scripts"), ("Scripts", "Data")
+            ("main", "Scripts"), ("Scripts", "Data"),
+            ("Scripts", "Settings"), ("Settings", "Buttons"),
+            ("Settings", "Instruments"), ("Instruments", "utils"),
         ])
 
         # Позиционирование
